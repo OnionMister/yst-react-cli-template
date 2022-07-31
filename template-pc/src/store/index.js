04d5777914e8ddf20cfc919771ae2f8091ccc577
yangshaotong <yangshaotong@xdfzx.com>
2022-07-31 23:40:39 +0800

    feat: template-pc的store划分model

--- template-pc/src/store/index.js
+++ template-pc/src/store/index.js
@@ -1,13 +1,11 @@
 import { init } from '@rematch/core';
 import createLoadingPlugin from '@rematch/loading';
-import home from './home';
+import * as models from './models';
 
 const loading = createLoadingPlugin({});
 
 const store = init({
-    models: {
-        home,
-    },
+    models,
     plugins: [loading],
 });
 export default store;
