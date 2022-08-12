4f7c2c64693e97b143c55f56ea10bb49a24b50bb
yangshaotong <yangshaotong@xdfzx.com>
2022-08-12 16:05:43 +0800

    feat: template-pc增加loading

--- template-pc/src/pages/Home/index.js
+++ template-pc/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import classBind from 'classnames/bind';
+import { Spin } from 'antd';
 import { connect } from 'react-redux';
 import { Button } from 'antd';
 import imgSrc from 'assets/img/dog.jpeg';
@@ -7,7 +8,7 @@ import List from './components/List';
 import styles from './style.less';
 
 const cx = classBind.bind(styles);
-const Home = ({ message, getCourseList }) => {
+const Home = ({ message, getCourseList, getCourseListLoading }) => {
     const [count, setCount] = useState(1);
     const [courseList, setCourseList] = useState([]);
     useEffect(() => {
@@ -16,22 +17,29 @@ const Home = ({ message, getCourseList }) => {
         });
     }, []);
     return (
-        <div className={cx('home')}>
-            <img src={imgSrc}></img>
-            <div>欢迎使用yst-react-cli</div>
-            <div>{message}</div>
-            <Button
-                type='primary'
-                onClick={() => setCount(count + 1)}
-            >count + 1
-            </Button>
-            <div>我是useState数据：{count}</div>
-            <List courseList={courseList}/>
-        </div>
+        <Spin
+            tip='loading...'
+            spinning={getCourseListLoading}
+            wrapperClassName={cx('home__spin')}
+        >
+            <div className={cx('home')}>
+                <img src={imgSrc}></img>
+                <div>欢迎使用yst-react-cli</div>
+                <div>{message}</div>
+                <Button
+                    type='primary'
+                    onClick={() => setCount(count + 1)}
+                >count + 1
+                </Button>
+                <div>我是useState数据：{count}</div>
+                <List courseList={courseList}/>
+            </div>
+        </Spin>
     );
 };
 
-const mapStateToProps = ({ home }) => ({
+const mapStateToProps = ({ home, loading }) => ({
+    getCourseListLoading: loading.effects.home.getCourseList,
     ...home,
 });
 const mapDispatchToProps = (dispatch) => {
