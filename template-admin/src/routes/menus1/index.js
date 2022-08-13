65cbd4aceeba230bac5b88126ae011e05123487c
yangshaotong <yangshaotong@xdfzx.com>
2022-08-13 15:45:15 +0800

    feat: template-admin示例菜单1-1路由优化

--- template-admin/src/routes/menus1/index.js
+++ template-admin/src/routes/menus1/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { loadableLazy } from 'utils';
-import { Navigate } from 'react-router';
+import { Navigate, Outlet } from 'react-router';
 
 export default [
     {
         path: 'menus1',
         breadcrumb: '菜单1',
-        element: loadableLazy(() => import('pages/Home')),
+        element: <Outlet />,
         children: [
             {
                 index: true,
-                element: <Navigate to="/menus1-1" />,
+                element: <Navigate to="menus1-1" />,
             },
             {
                 path: 'menus1-1',
